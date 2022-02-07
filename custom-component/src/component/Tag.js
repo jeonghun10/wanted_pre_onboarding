c32c92b7048b0944762ad8ac7fc938c861343ef9
jeonghun10 <jeonghun8910@gmail.com>
2022-02-07 22:22:15 +0900

    client:Tab menu

---
+++ custom-component/src/component/Tag.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+
+const Tag = () => {
+  const [currentTab, setCurrentTab] = useState(0)
+
+  const menuArr = [
+      {name:'tab1', content:'Tab menu One'},
+      {name:'tab2', content:'Tab menu Two'},
+      {name:'tab3', content:'Tab menu Three'},
+  ]
+
+  const TabHandler = (index) => {
+    setCurrentTab(index)
+  }
+  return (
+    <div className="tabWrab">
+      <h1>Tab</h1>
+        <ul className="tabMenu">
+        {
+            menuArr.map((el, index)=>{
+                return (
+                    <li onClick={()=>TabHandler(index)} key={index} className={currentTab===index ? 'submenu focused':'submenu'}>
+                        {el.name}                        
+                    </li>
+                )
+            })
+        }
+        </ul>
+        <div>{menuArr[currentTab].content}</div>
+    </div>
+  );
+};
+
+export default Tag;
+
+
